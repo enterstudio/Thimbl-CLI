9f4e59fa78deea308dd30f406c758ee451c7faf4
Mark Carter <mcturra2000@yahoo.co.uk>
2011-02-18 16:52:48 +0000

    setup improvements

--- thimbl.py
+++ thimbl.py
@@ -1,11 +1,14 @@
 '''thimbl.py - Command-line python tools '''
 
+import copy
 import cStringIO
 import datetime
+import getpass
 import json
 import pdb
 import optparse
 import os
+import platform
 import re
 import subprocess
 import sys
@@ -21,12 +24,24 @@ def writeln(text):
 
 class Data:
     def __init__(self):
-        self.load_cache()
-        #print self.data
-        #pdb.set_trace()
-        self.whoami = self.data['me']
-        self.me = self.data['plans'][self.whoami]
-    
+
+        # Try to load the cache file, if it exists
+        thimblfile = self.__cache_filename()
+        if os.path.isfile(thimblfile):
+            data = load(thimblfile)
+        else:
+            print 'Configuration was guessed. You should run the commands'
+            print "'info' or 'setup' to put in proper values"
+            data = self.empty_cache()
+
+        self.set_data(data)
+
+    def set_data(self, data):
+        self.whoami = data['me']
+        self.data = data
+        self.me = data['plans'][self.whoami]
+            
+
     def __cache_filename(self):
         'Return the name of the cache file that stores all of the data'
         thimbldir = os.path.expanduser('~/.config/thimbl')
@@ -38,19 +53,37 @@ class Data:
     def info(self):
         'Print some information about thimbl-cli'
         print "cache_filename: " + self.__cache_filename()
-        print "name:           " + self.me['name']
-        print "address:        " + self.me['address']
-        print "email:          " + self.me['properties']['email']
-        print "mobile:         " + self.me['properties']['mobile']
-        print "website:        " + self.me['properties']['website']
+        me = self.me
+        print "name:           " + me['name']
+        print "address:        " + me['address']
+        props =  me['properties']
+        print "email:          " + props['email']
+        print "mobile:         " + props['mobile']
+        print "website:        " + props['website']
 
-    def load_cache(self):
-        'Load the data file'
-        thimblfile = self.__cache_filename()
-        if os.path.isfile(thimblfile):
-            self.data = load(thimblfile)
-        else:
-            self.setup()
+    def empty_cache(self):
+        'Create a blank config cache, populating it with sensible defaults'
+
+        host = platform.node()
+        name = getpass.getuser() # user name
+        email = '{0}@{1}'.format(name, host)
+        address = email
+
+        properties =  {}
+        properties['website'] = 'http://' + host
+        properties['mobile']  = 'Mobile withheld'
+        properties['email'] = email
+
+        plan = {}
+        plan['address'] = address
+        plan['name'] = name
+        plan['messages'] = []
+        plan['replies'] = {}
+        plan['following'] = []
+        plan['properties'] = properties
+
+        return { 'me' : address, 'plans' : { address : plan }}
+
 
     def save_cache(self):
         cache_file = self.__cache_filename()
@@ -120,11 +153,13 @@ class Data:
         for msg in messages:
             # format time
             t = str(msg['time'])
-            tlist = map(int, [t[:4], t[4:6], t[6:8], t[8:10], t[10:12], t[12:14]])
+            tlist = map(int, [t[:4], t[4:6], t[6:8], t[8:10], 
+                              t[10:12], t[12:14]])
             tstruct = apply(datetime.datetime, tlist)
             ftime = tstruct.strftime('%Y-%m-%d %H:%M:%S')
 
-            text = '{0}  {1}\n{2}\n\n'.format(ftime, msg['address'], msg['text'].encode('utf-8'))
+            text = '{0}  {1}\n{2}\n\n'.format(ftime, msg['address'], 
+                                              msg['text'].encode('utf-8'))
             wout(text)
         
         
@@ -137,17 +172,40 @@ class Data:
 
 
     
-    def setup(self, values = None):
-        def create(address, bio, name, website, mobile, email):
-            'Create data given user information'
-            properties =  {'website' : website, 'mobile' : mobile, 'email' : email }
-            plan = { 'address' : address, 'name' : name, 'messages' : [],
-                     'replies' : {},'following' : [], 'properties' : properties}
-            data = { 'me' : address, 'plans' : { address : plan }}
-            return data
-        if not values:
-            values = ["me@example.com", "I am not a number", "6", "www.example.com", "TBD", "me@example.com"]
-        self.data = apply(create, values)
+    def setup(self):
+        'Interactively enter user information'
+        def getval(v, prompt):
+            while True:
+                print prompt + '=' + v+ ' [Accept (default)/Change/Erase]? '
+                inp = raw_input()
+                if inp in ['A', 'a', '']:
+                    return v
+                elif inp in ['C', 'c']:
+                    print 'Input new value: ',
+                    v = raw_input()
+                    return v
+                elif inp in ['E', 'e']:
+                    return ''
+                else:
+                    print "Didn't understand your response"
+
+        me = copy.copy(self.me)
+        del self.data['plans'][self.whoami]
+        me['name'] = getval(me['name'], 'Name')
+        address = getval(me['address'], 'Address')
+        me['address'] = address
+
+        props = me['properties']
+        props['website'] = getval(props['website'], 'Website')
+        props['mobile'] = getval(props['mobile'], 'Mobile')
+        props['email'] = getval(props['email'], 'Email')
+        me['properties'] = props
+
+        self.data['plans'][address] = me
+        self.data['me'] = address
+        self.set_data(self.data)
+
+
 
     def unfollow(self, address):
         'Remove an address from someone being followed'
@@ -241,7 +299,7 @@ def main():
         d.fetch()
         d.prmess()
     elif cmd == 'setup':
-        d.setup(sys.argv[2:])
+        d.setup()
     elif cmd == 'stdin':
         text = sys.stdin.read()
         d.post(text)
